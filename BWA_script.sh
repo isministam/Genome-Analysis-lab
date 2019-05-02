df4bc61e3865f08bfbf5dc2b2fb4666543449529
isministam <stamatelouismini@gmail.com>
2019-05-02 16:23:08 +0200

    Script for BWA and samtools

---
+++ BWA_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 04:00:00
+#SBATCH -J BWA
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user stamatelouismini@gmail.com
+
+# Load modules
+module load bioinfo-tools
+module load bwa
+module samtools/1.9
+
+# Your commands
+bwa index -p pacbio_index Enterococci.contigs.fasta
+bwa mem pacbio_index E745-1.L500_SZAXPI015146-56_1_clean.fq.gz E745-1.L500_SZAXPI015146-56_2_clean.fq.gz > aln-pe.sam | samtools sort -o aln-pe.bam
+
+
