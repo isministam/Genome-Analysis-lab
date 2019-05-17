4de4a76f0d0bca295c9fcc784bfc5307df6df396
isministam <stamatelouismini@gmail.com>
2019-05-17 15:41:48 +0200

    New count scripts for BH and Serum and fna file for related genome.

--- Count_BH/script_count_BH.sh
+++ Count_BH/script_count_BH.sh
@@ -2,20 +2,20 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 08:00:00
+#SBATCH -t 03:00:00
 #SBATCH -J Count_reads_BH
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user stamatelouismini@gmail.com
 
 # Load modules
 module load bioinfo-tools
-module load samtools/1.9
 module load htseq
 
 # Your commands
 
 
-samtools view out_BH_Map_ERR1797972.bam | htseq-count -f bam --type CDS out_BH_Map_ERR1797972.bam PROKKA_04122019.gtf > out_1797972.txt
-samtools view out_BH_Map_ERR1797973.bam | htseq-count -f bam --type CDS out_BH_Map_ERR1797973.bam PROKKA_04122019.gtf > out_1797973.txt
-samtools view out_BH_Map_ERR1797974.bam | htseq-count -f bam --type CDS out_BH_Map_ERR1797974.bam PROKKA_04122019.gtf > out_1797974.txt
+htseq-count -f bam -t CDS -i ID -r pos out_BH_Map_ERR1797972.bam PROKKA_05072019.gff > out_1797972.count
+#htseq-count --stranded=reverse -f bam -t CDS -r pos out_BH_Map_ERR1797972.bam PROKKA_04122019.gtf > out_1797972.count
+htseq-count -f bam -t CDS -i ID -r pos out_BH_Map_ERR1797973.bam PROKKA_05072019.gff > out_1797973.count
+htseq-count -f bam -t CDS -i ID -r pos out_BH_Map_ERR1797974.bam PROKKA_05072019.gff > out_1797974.count
 
--- Count_Serum/script_count_Serum.sh
+++ Count_Serum/script_count_Serum.sh
@@ -2,7 +2,7 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 08:00:00
+#SBATCH -t 03:00:00
 #SBATCH -J Count_reads_Serum
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user stamatelouismini@gmail.com
@@ -15,9 +15,9 @@ module load htseq
 # Your commands
 
 
-samtools view out_BH_Map_ERR1797969.bam | htseq-count -f bam --type CDS out_BH_Map_ERR1797969.bam PROKKA_04122019.gtf > out_1797969.txt
-samtools view out_BH_Map_ERR1797970.bam | htseq-count -f bam --type CDS out_BH_Map_ERR1797970.bam PROKKA_04122019.gtf > out_1797970.txt
-samtools view out_BH_Map_ERR1797971.bam | htseq-count -f bam --type CDS out_BH_Map_ERR1797971.bam PROKKA_04122019.gtf > out_1797971.txt
+samtools view out_Serum_Map_ERR1797969.bam | htseq-count -f bam -t CDS -i ID -r pos out_Serum_Map_ERR1797969.bam PROKKA_05072019.gff > out_1797969.count
+samtools view out_Serum_Map_ERR1797970.bam | htseq-count -f bam -t CDS -i ID -r pos out_Serum_Map_ERR1797970.bam PROKKA_05072019.gff > out_1797970.count
+samtools view out_Serum_Map_ERR1797971.bam | htseq-count -f bam -t CDS -i ID -r pos out_Serum_Map_ERR1797971.bam PROKKA_05072019.gff > out_1797971.count
 
 
 
